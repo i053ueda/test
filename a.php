f6866526b6fb3dd58dfa2fc600811ebc25c953f0
i053ueda <138637043+i053ueda@users.noreply.github.com>
2023-07-12 10:45:04 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php 
-  foreach(file('/usr/share/dict/linux.words') as $line){
+  foreach(file('/usr/share/dict/words') as $line){ //wordの前のlinax.を削除した
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
   } 
 ?>
