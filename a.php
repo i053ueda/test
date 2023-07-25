483ec0401e69c49e5031338225a781f73e6715e0
i053ueda <138637043+i053ueda@users.noreply.github.com>
2023-07-25 15:29:24 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,9 @@
 #!/usr/bin/php
-<?php 
+<?php
   foreach(file('/usr/share/dict/words') as $line){ //wordの前のlinax.を削除した
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-  } 
+    if( strpos($line, "xy") !== false ){ //strpos($line, "xy")によってxyを含む単語を検索した
+        echo "$line";
+    }
+  }
 ?>
